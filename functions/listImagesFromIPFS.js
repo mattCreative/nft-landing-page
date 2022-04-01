4a6313f92d5e296492225241be5a374f1fa245c1
Matthew Jones <matt@mattCreative.ca>
2022-03-31 22:30:32 -0400

    Missing parameters

--- functions/listImagesFromIPFS.js
+++ functions/listImagesFromIPFS.js
@@ -7,6 +7,7 @@ exports.handler = async (event, context) => {
 
 	let options = {
 		method: 'GET',
+		qs: {type: 'all'},
 		headers: {
 			'Content-Type': 'application/json',
 			Authorization: AUTH
