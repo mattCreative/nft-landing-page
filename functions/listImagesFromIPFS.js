075c24fcecb08d32ebdc73ff8bfd1393a9d82aa2
Matthew Jones <matt@mattCreative.ca>
2022-04-01 12:06:50 -0400

    Swap out with async/wait

--- functions/listImagesFromIPFS.js
+++ functions/listImagesFromIPFS.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 const AUTH = process.env.NFTPORT_AUTH;
 
 exports.handler = async (event, context) => {
-	const url = 'https://api.nftport.xyz/v0/me/storage';
+	
 	/*
 	let options = {
 		method: 'GET',
@@ -28,7 +28,7 @@ exports.handler = async (event, context) => {
 		.catch(err => console.error('error:' + err));
 	*/
 
-	let response = "test";
+	const response = await getListOfImages()
 	
 	return {
 		'statusCode': 200,
@@ -39,4 +39,51 @@ exports.handler = async (event, context) => {
 		'body': JSON.stringify(response)
 	}
   
+}
+
+const getListOfImages = async () => {
+	const url = 'https://api.nftport.xyz/v0/me/storage';
+  
+  const options = {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: AUTH
+    }
+  };
+  
+  const query = new URLSearchParams({
+    type: 'all'
+  });
+
+  let editions = []
+  try {
+    const data = await fetchData(url + query, options)
+	
+    return {
+      data: data,
+    }
+  } catch(err) {
+    console.log(`Catch: ${JSON.stringify(err)}`)
+    return {
+      error: err
+    }
+  }
+}
+
+async function fetchData(url, options) {
+  return new Promise((resolve, reject) => {
+    return fetch(url, options).then(res => {
+      const status = res.status;            
+
+      if(status === 200) {
+        return resolve(res.json());
+      } else {
+        console.log(`Fetch failed with status ${status}`);
+        return reject(res.json());
+      }        
+    }).catch(function (error) { 
+      reject(error)
+    });
+  });
 }
