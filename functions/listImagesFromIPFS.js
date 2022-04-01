153ae1c1ea572ed45f33a95940b826fa652b6bac
Matthew Jones <matt@mattCreative.ca>
2022-03-31 22:25:41 -0400

    URL fix

--- functions/listImagesFromIPFS.js
+++ functions/listImagesFromIPFS.js
@@ -3,8 +3,7 @@ const fetch = require('node-fetch');
 const AUTH = process.env.NFTPORT_AUTH;
 
 exports.handler = async (event, context) => {
-	const url = "https://api.nftport.xyz/v0/files";
-	let url = 'https://api.nftport.xyz/v0/me/storage';
+	const url = 'https://api.nftport.xyz/v0/me/storage';
 
 	let options = {
 		method: 'GET',
